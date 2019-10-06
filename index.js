6f2f964f9f5bf7699450a21f4adb9b3cb90025af
BuildTools <unconfigured@null.spigotmc.org>
2019-10-06 15:45:47 -0700

    ss

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === 'snacks') {
     msg.reply('SNACKS!!!! these bitches use to be bussin! They use to think i was plain hide n seek but i was really in the bushes eatin dese mfs')
-	message.channel.send('SNACKS')
+
   }
   if (msg.content === 'walter') {
     msg.reply('i like fire trucks and moster trucks')
