eea61d8f94ab0908f3f93e4090773e0ab777a0b6
BuildTools <unconfigured@null.spigotmc.org>
2019-10-06 14:43:50 -0700

    make it better

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'snacks') {
+    msg.reply('SNACKS!!!! these bitches use to be bussin! They use to think i was plain hide n seek but i was really in the bushes eatin dese mfs')
+  }
+})
+
+client.login('NjMwNTEwNDY5MTg2MzIyNDQy.XZpXxw.8_tadFPsp35chyceXnHfApl5fp4')
