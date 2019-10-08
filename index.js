c9278484d33c1d41adfcc230ff2e1c69d1bcb09c
BrandtWP <48268950+BrandtWP@users.noreply.github.com>
2019-10-07 17:12:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,43 +19,46 @@ const rand_doge = [
 ]
 
 client.on('message', message => {
-	if (message.content.includes("snacks")) {
+	
+	var msg = message.content
+	
+	if (msg.toLowerCase().includes("snacks")) {
 		message.channel.send('*SNACKS!!!! these bitches use to be bussin! They use to think i was plain hide n seek but i was really in the bushes eatin dese mfs*');
 	}
-	if (message.content === 'walter') {
+	if (msg === 'walter') {
 		const Xwalter = client.emojis.get("608709784257953814");
 		message.channel.send('**I LIKE FIRE TRUCKS AND MOSTER TRUCKS**');
 		message.channel.send('<:Xwalter:608709784257953814>');
 		message.channel.send('**WALTER**');
 	}
-	if (message.content.startsWith(prefix + "brandt")) {
+	if (msg.startsWith(prefix + "brandt")) {
 		var randomAnswer = rando_emoji[Math.floor(Math.random() * rando_emoji.length)];
 		message.channel.send(randomAnswer);
 	}
-	if (message.content.startsWith(prefix + "twinkie")) {
+	if (msg.startsWith(prefix + "twinkie")) {
 		var randomDoge = rand_doge[Math.floor(Math.random() * rand_doge.length)];
 		message.channel.send(randomDoge);
 	}
 	
-	if (message.content.includes("mood")) {
+	if (msg.content.includes("mood")) {
 		if(message.author.bot) return;
 		var randomAnswer = rando_emoji[Math.floor(Math.random() * rando_emoji.length)];
 		message.channel.send("The mood is: " + randomAnswer);
 	}
 	
-	if (message.content === "listemojis") {
+	if (msg === "listemojis") {
 		const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
 		message.channel.send(emojiList);
 }
 	/*if (message.content.includes("brandt")) {
 		message.channel.send('<:Xwalter:608709784257953814>');
 	} */
-	if (!message.content.startsWith(prefix)) return;
+	if (!msg.startsWith(prefix)) return;
  
-	if (message.content.startsWith(prefix + "ping")) {
+	if (msg.startsWith(prefix + "ping")) {
 		message.channel.send("pong!");
 	}
-	if (message.content.startsWith(prefix + "avatar")) {
+	if (msg.startsWith(prefix + "avatar")) {
 		// Send the user's avatar URL
 		message.channel.send(message.author.avatarURL);
 	}
